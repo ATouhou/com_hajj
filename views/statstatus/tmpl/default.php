6685f62febe9bb8b0199df13c87282dfcc1f1576
kouceyla <hadjikouceyla@gmail.com>
2014-09-04 12:32:44 +0100

    Change link in stat status dash Fixes #129

--- views/statstatus/tmpl/default.php
+++ views/statstatus/tmpl/default.php
@@ -18,7 +18,10 @@ $th = '';
 $td = '';
 foreach ($data as $key => $value) {
   $th .= '<th>'.$status_hajjs[$value->register_status-1].'</th>';
-  $td .= '<td><a href="index.php?option=com_hajj&task=admin.hajjs&Itemid=241&register_status='.$value->register_status.'">'.$value->count.'</a></td>';
+  $url = ($value->register_status == 3 || $value->register_status == 5)? 
+    'index.php?option=com_hajj&task=admin.hajjs&deny=1&Itemid=289' :
+    'index.php?option=com_hajj&task=admin.hajjs&Itemid=241&register_status='.$value->register_status.'';
+  $td .= '<td><a href="'.$url.'">'.$value->count.'</a></td>';
 }
 
 ?>
