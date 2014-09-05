c3e7158559f9e6ef438ff512dbb7e5b82553c6e9
kouceyla <hadjikouceyla@gmail.com>
2014-09-05 22:14:46 +0100

    only to check cannot see the price Fixes #133

--- views/topay/tmpl/default.php
+++ views/topay/tmpl/default.php
@@ -11,17 +11,18 @@ defined('_JEXEC') or die;
 $hajj = $this->hajj;
 $parent = $this->parent;
 
+
 ?>
 
 <?php if ($parent==""): // This is the parent ?>
-  <?php if ($hajj->register_status == 2): // Accepted ?>
+  <?php if ($hajj->register_status != 1): // Accepted ?>
     <h2> رقم الحجز : <?php echo $hajj->id ?> </h2>
     <h3>مبلغ الحجز : <?php echo $hajj->topay ?> ريال</h3>
   <?php else: // Not accepted ?>
     <h3>لا يمكنكم الإطلاع على طريقة الدفع حتى تتم مراجعة طلبكم</h3>
   <?php endif ?>
 <?php else: // This is the Addon?>
-   <?php if ($parent->register_status == 2): // Accepted ?>
+   <?php if ($parent->register_status != 1): // Accepted ?>
     <h2> رقم الحجز : <?php echo $hajj->id ?> </h2>
     <h3>المبلغ المطلوب <?php echo $parent->topay ?> وسيتم سداده من رقم الحجز الأصل : <?php echo $parent->id ?></h3>
   <?php else: // Not accepted ?>
