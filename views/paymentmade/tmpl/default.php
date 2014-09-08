61fdc03e72a0949627aa9e9dc092216a19b618a3
kouceyla <hadjikouceyla@gmail.com>
2014-09-08 18:29:20 +0100

    Delet button ADD from payment made Fixes #188

--- views/paymentmade/tmpl/default.php
+++ views/paymentmade/tmpl/default.php
@@ -28,23 +28,6 @@ $sexe = array(
   'f' => 'نساء',
   );
 ?>
-<div class="accordion" id="accordion2">
-  <div class="accordion-group">
-    <div class="accordion-heading">
-      <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
-        <span class="btn">اضافة</span>
-      </a>
-    </div>
-    <div id="collapseOne" class="accordion-body collapse">
-      <div class="accordion-inner">
-      
-      <?php
-        HajjFieldHelper::getFormHajj();
-      ?>
-      </div>
-    </div>
-  </div>
-</div>
 <h1>طلبات تم الدفع</h1>
 <?php echo $ThePager ?>
 <?php echo $ThePagination; ?>
